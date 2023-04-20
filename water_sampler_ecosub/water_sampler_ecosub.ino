17e89fde46dabfcaec01af0b65147914b5618ddd
Jordan <jordan.williams@nephele.uk>
2023-04-20 02:42:03 +0100

    Added rs232 to the inteface port

--- water_sampler_ecosub/water_sampler_ecosub.ino
+++ water_sampler_ecosub/water_sampler_ecosub.ino
@@ -1,3 +1,9 @@
+// Water Sampler Software
+// University of Southampton - GDP32
+// Year 2023
+// Author Jordan Williams
+// Supervisor Adrian Nightingale
+
 // Includes
 #include <Wire.h>
 #include <Servo.h>
@@ -14,8 +20,9 @@
 
 // Global constants
 // Do not edit these
-const String VERSION = "v2.1";
+const String VERSION = "v2.2";
 
+const int LED_PIN = 10;
 const int SERVO_PIN = 9;
 const int COMM_PIN_1 = A4;
 const int COMM_PIN_2 = A5;
@@ -29,25 +36,43 @@ float servoPosition = 0;
 
 Servo samplerServo;
 
+SoftwareSerial samplerSerial(COMM_PIN_1, COMM_PIN_2);
+
 void setup (){
+  int i;
+  // Power Up LED
+  pinMode(LED_PIN, OUTPUT);
+  for (i = 0; i < 3; i++){
+    digitalWrite(LED_PIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+    delay(500);                      // wait for a second
+    digitalWrite(LED_PIN, LOW);   // turn the LED off by making the voltage LOW
+    delay(500);        
+  }
   // Servo Setup
   samplerServo.attach(SERVO_PIN);
-  samplerServo.write(servoPosition);
+  resetSampler();
 
-  if (COMM_PROTO == "rs232" || COMM_PROTO == "terminal"){
+  if (COMM_PROTO == "terminal"){
     // Open serial communications and wait for port to open:
     Serial.begin(BAUD_RATE);
     while (!Serial) {
       ; // wait for serial port to connect. Needed for Native USB only
     }
-    if (COMM_PROTO == "terminal"){
-      Serial.print("Water Sampler, Software Version ");
-      Serial.println(VERSION);
-      Serial.println("Options:");
-      Serial.println("\t$sample - Takes Sample");
-      Serial.println("\t$reset  - Reset Sampler");
-      Serial.println(">>> ");
-    }
+    Serial.print("Water Sampler, Software Version ");
+    Serial.println(VERSION);
+    Serial.println("Options:");
+    Serial.println("\t$sample - Takes Sample");
+    Serial.println("\t$reset  - Reset Sampler");
+    Serial.println(">>> ");
+  }
+
+  if (COMM_PROTO == "rs232"){
+    // Define pin modes for TX and RX
+    pinMode(COMM_PIN_1, INPUT);
+    pinMode(COMM_PIN_2, OUTPUT);
+    
+    // Set the baud rate for the SoftwareSerial object
+    samplerSerial.begin(9600);
   }
 
   if (COMM_PROTO == "i2c"){
@@ -62,19 +87,17 @@ void setup (){
 }
 
 void loop (){
-  if (COMM_PROTO == "rs232" || COMM_PROTO == "terminal"){
+  if (COMM_PROTO == "terminal"){
     while (Serial.available() == 0) {}     //wait for data available
     String recievedCommand = Serial.readString();  //read until timeout
     recievedCommand.trim();                        // remove any \r \n whitespace at the end of the String
     if (recievedCommand == "$sample") {
       collectSample();
       Serial.println("$ok"); // Currently sends okay regardless of if sample is collected or not
-      if (COMM_PROTO == "terminal"){
-        Serial.print("Sample Collected: ");
-        Serial.print(collectedSamples);
-        Serial.print("/");
-        Serial.println(SAMPLES);
-      }
+      Serial.print("Sample Collected: ");
+      Serial.print(collectedSamples);
+      Serial.print("/");
+      Serial.println(SAMPLES);
     } 
     if (recievedCommand == "$reset") {
       // Reset all values and put servo to zero position
@@ -82,9 +105,26 @@ void loop (){
       samplerServo.write(servoPosition);
       collectedSamples = 0;
       Serial.println("$ok");
-      if (COMM_PROTO == "terminal"){
-        Serial.println("Sampler Reset");
-      }
+      Serial.println("Sampler Reset");
+    }
+  }
+
+  if (COMM_PROTO == "rs232"){
+    while (samplerSerial.available() == 0) {}     //wait for data available
+    String recievedCommand = samplerSerial.readString();  //read until timeout
+    recievedCommand.trim();                        // remove any \r \n whitespace at the end of the String
+    if (recievedCommand == "$sample") {
+      collectSample();
+      samplerSerial.println("$ok"); // Currently sends okay regardless of if sample is collected or not
+    } 
+    if (recievedCommand == "$reset") {
+      // Reset all values and put servo to zero position
+      resetSampler();
+      samplerSerial.println("$ok");
+    }
+    if (recievedCommand == "$state") {
+      // Heartbeat command
+      samplerSerial.println("$ok");
     }
   }
 
@@ -102,4 +142,13 @@ void collectSample(){
   if (COMM_PROTO == "i2c"){
     Wire.write("$ok");
   }
+  if (collectedSamples == 6){
+    resetSampler();
+  }
+}
+
+void resetSampler(){
+  servoPosition = 0.0;
+  samplerServo.write(servoPosition);
+  collectedSamples = 0;
 }
